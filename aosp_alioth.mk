29ec371b725cee28554afb8656d65888a31b9717
NextWork123 <nextdesigns@libero.it>
2022-01-14 17:42:01 +0100

    Alioth: Bring-up Project Elixir

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -8,20 +8,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelPlus-UI stuff.
+# Inherit some common  Project Elixir stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 WITH_GAPPS := true
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_BOOT_ANIMATION_RES := 1440
 
-# PixelPlus-UI Official Stuff
-CUSTOM_BUILD_TYPE := OFFICIAL
+# Project Elixir Official Stuff
+Elixir_MAINTAINER := NextWorks 
 IS_PHONE := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-PPUI_MAINTAINER := johnmart19
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
